71c8ced597d011c22a0624a44dc549d3dc43f25b
Jakub Szkiłądź <jakub.szkiladz@wp.pl>
2022-03-28 21:24:05 +0200

    organized ui

--- MtgHelper/mainwindow.cpp
+++ MtgHelper/mainwindow.cpp
@@ -6,10 +6,63 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    networkManager = new QNetworkAccessManager(this);
+    reply = nullptr;
+    dataBuffer = new QByteArray();
+
+    connect(ui->buttonSearch,&QToolButton::pressed, this, &MainWindow::Search);
 }
 
 MainWindow::~MainWindow()
 {
+    delete networkManager;
+    delete dataBuffer;
+
     delete ui;
 }
 
+void MainWindow::Search()
+{
+    QNetworkRequest req{QUrl(this->url)};
+    reply = networkManager->get(req);
+    connect(reply, &QNetworkReply::readyRead, this, &MainWindow::ReadData);
+    connect(reply, &QNetworkReply::finished,  this, &MainWindow::FinishReading);
+}
+
+void MainWindow::ReadData()
+{
+    dataBuffer->append(reply->readAll());
+}
+
+void MainWindow::FinishReading()
+{
+    if(reply->error() != QNetworkReply::NoError)
+    {
+        qDebug() << "Error : " << reply->errorString();
+        //QMessageBox::warning(this,"Error",QString("Request[Error] : %1").arg(netReply->errorString()));
+    }
+    else
+    {
+
+        //CONVERT THE DATA FROM A JSON DOC TO A JSON OBJECT
+        QJsonObject userJsonInfo = QJsonDocument::fromJson(*dataBuffer).object();
+        QJsonObject card = userJsonInfo.find("card")->toObject();
+
+        //SET TEXT
+//        QString name = card.value("name").toString();
+//        QString type = card.value("type").toString();
+//        QString text = card.value("text").toString();
+
+//        ui->labelName->setText(name);
+//        ui->labelType->setText(type);
+//        ui->labelText->setText(text);
+
+        //SET PICTURE
+//        auto picLink = userJsonInfo.value("avatar_url").toString();
+//        QNetworkRequest link{QUrl(picLink)};
+//        reply = netManager->get(link);
+//        connect(reply,&QNetworkReply::finished,this,&MainWindow::setUserImage);
+        dataBuffer->clear();
+    }
+}
--- MtgHelper/mainwindow.h
+++ MtgHelper/mainwindow.h
@@ -2,6 +2,16 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QUrl>
+
+#include <QJsonDocument>
+#include <QJsonValue>
+#include <QJsonArray>
+#include <QJsonObject>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +27,31 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+    // https://api.magicthegathering.io/v1/cards/?name=nissa&cmc=5
+    // https://api.magicthegathering.io/v1/cards/?name=avacyn
+    const QString url = "https://api.magicthegathering.io/v1/cards/386616";
+    QNetworkAccessManager *networkManager;
+    QNetworkReply *reply;
+    QByteArray *dataBuffer;
+
+    void Search();
+    void ReadData();
+    void FinishReading();
 };
 #endif // MAINWINDOW_H
+
+
+
+/*!
+ * linki:
+ *      - https://dev.to/truepadawan/qt-c-working-with-rest-api-35fe
+ *      - https://docs.magicthegathering.io/
+ *      - https://scryfall.com/docs/api
+ *
+ * wizja:
+ *      - na start wpisywanie nazywy i pobieranie z api
+ *      - zapis do bazy
+ *      - jeżeli karta jest już w bazie to nie pobieraj
+ *      - updatowanie bazy w zależności czy karta jest zapisana?
+ */
